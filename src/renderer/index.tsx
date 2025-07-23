98dd249d1c05602984e0a4fcb5a6d13e0dae6dd2
mariomamo <45656028+mariomamo@users.noreply.github.com>
2025-07-24 00:02:51 +0200

    Added kamaji page

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -3,60 +3,34 @@
  * Licensed under MIT License. See LICENSE in root directory for more information.
  */
 
-// transpiled .tsx code must have `React` symbol in the scope
 // @ts-ignore
-import React from "react";
 
 import { Renderer } from "@freelensapp/extensions";
 import { KamajiPreferencesStore } from "../common/store";
-import { KamajiDetails } from "./details/kamaji-details";
-import { KamajiIcon } from "./icons";
-import { Kamaji } from "./k8s/kamaji";
-import { KamajisPage } from "./pages";
-import { KamajiPreferenceHint, KamajiPreferenceInput } from "./preferences";
+import { KamajiIcon } from "./icons/kamaji";
+import {MainPage} from "./pages/main/main-page";
 
 export default class KamajiRenderer extends Renderer.LensExtension {
   async onActivate() {
     await KamajiPreferencesStore.getInstanceOrCreate().loadExtension(this);
   }
 
-  appPreferences = [
+  clusterPageMenus = [
     {
-      title: "Kamaji Preferences",
+      id: "kamaji-icon",
+      title: "Kamaji",
+      target: { pageId: "kamaji-main-page" },
       components: {
-        Input: () => <KamajiPreferenceInput />,
-        Hint: () => <KamajiPreferenceHint />,
-      },
-    },
-  ];
-
-  kubeObjectDetailItems = [
-    {
-      kind: "Kamaji",
-      apiVersions: ["kamaji.freelens.app/v1alpha1"],
-      priority: 10,
-      components: {
-        Details: (props: Renderer.Component.KubeObjectDetailsProps<Kamaji>) => <KamajiDetails {...props} />,
+        Icon: KamajiIcon,
       },
     },
   ];
 
   clusterPages = [
     {
-      id: "examples-page",
+      id: "kamaji-main-page",
       components: {
-        Page: () => <KamajisPage extension={this} />,
-      },
-    },
-  ];
-
-  clusterPageMenus = [
-    {
-      id: "kamaji",
-      title: "Kamaji",
-      target: { pageId: "examples-page" },
-      components: {
-        Icon: KamajiIcon,
+        Page: () => <MainPage />,
       },
     },
   ];
--- src/renderer/k8s/kamaji/types.ts
+++
@@ -1,4 +0,0 @@
-export interface NamespacedObjectReference {
-  name: string;
-  namespace?: string;
-}
