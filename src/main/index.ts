f3bf906f0fca7bf9beed8cf4498e855d0e3ff703
mariomamo <45656028+mariomamo@users.noreply.github.com>
2025-08-09 01:18:40 +0200

    Created table for tenants

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,8 +1,6 @@
 import { Main } from "@freelensapp/extensions";
-import { KamajiPreferencesStore } from "../common/store";
 
 export default class KamajiMain extends Main.LensExtension {
   async onActivate() {
-    await KamajiPreferencesStore.getInstanceOrCreate().loadExtension(this);
   }
 }
